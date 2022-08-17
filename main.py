e5a4b81dd88fd39bb82831d2057029f575a2c77d
Pollyana <pollyanarocha416@gmail.com>
2022-08-17 18:10:31 -0300

    gerar idds

---
+++ main.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+#gerar id aleatorio
+def gerar_id():
+    qts_ids = 1
+    comprimento_do_id = 6
+    for x in range(qts_ids):
+        print(''.join(random.choice(string.ascii_letters + string.digits)
+              for _ in range(comprimento_do_id)))
+
+
+gerar_id()
+
+
+#gerar id apartir do que o user quer
+def user_id_gen_by_user():
+    qts_id = int(input("Quantidade de idds? "))
+    comprimento_id = int(input("comprimento deles? "))
+    for x in range(qts_id):
+        print(''.join(random.choice(string.ascii_letters + string.digits)
+              for _ in range(comprimento_id)))
+
+
+user_id_gen_by_user()
