014d41c754a3971420b821a82c4916f9b3ed4e59
Pollyana <pollyanarocha416@gmail.com>
2022-08-17 22:25:02 -0300

    atualizacao

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ gerar_id()
 
 #gerar id apartir do que o user quer
 def user_id_gen_by_user():
-    qts_id = int(input("Quantidade de idds? "))
-    comprimento_id = int(input("comprimento deles? "))
+    qts_id = int(input("Quantidade de ids que vc gostaria de gerar: "))
+    comprimento_id = int(input("comprimento dos ids: "))
     for x in range(qts_id):
         print(''.join(random.choice(string.ascii_letters + string.digits)
               for _ in range(comprimento_id)))
-
 
 user_id_gen_by_user()
